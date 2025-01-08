c2aa179bb3acbdb47a7b5a656ebafcfc4345237e
Ahmadfarhan67 <123234895+Ahmadfarhan67@users.noreply.github.com>
2025-01-08 22:05:05 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.clicky.pk/embroidered-sweatshirt-brackets?id=66279666e4300d6cbf3cf6ca";
-    const blackPageURL = "https://ifrxjwhbvb.myfunnelish.com/imbassd-1735569734063202-1735754211879216";
+    const whitePageURL = "https://www.ubuy.com.pk/en/productin/LUF3D2OAI-the-modern-soul-stripe-self-design-oversized-tshirt-for-men-oversize-t-shirts-for-men-men-drop-shoulder-tshirts-bagy-fit-mens-tshirt?srsltid=AfmBOoprVlk0Cx5Nm3aDD5AYfQiBoS2XlWcOiK8ENH8PQcLVCph1gzE9";
+    const blackPageURL = "https://ifrxjwhbvb.myfunnelish.com/imbassd-1735569734063202";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
