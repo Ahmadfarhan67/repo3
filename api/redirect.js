646214187fd0ab073edca82e62b590902194bb23
Ahmadfarhan67 <123234895+Ahmadfarhan67@users.noreply.github.com>
2025-01-16 23:02:22 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://vercel.com/ahmad-farhans-projects-00acab7a/repo3";
+    const whitePageURL = "https://www.teepublic.com/t-shirt/668456-i-was-left-unsupervised-white-text";
     const blackPageURL = "https://ifrxjwhbvb.myfunnelish.com/imbassd-1735569734063202-1735754211879216";
   
     // Parse the UTM parameters from the request URL
